b92ffee5085c1b9ed4598a9b573b9e9350d43e23
Gymnopedee <bsky01@gmail.com>
2026-05-31 09:14:32 +0900

    Fix TS2345 in firebase config

--- backend/src/config/firebase.ts
+++ backend/src/config/firebase.ts
@@ -11,8 +11,8 @@ if (!admin.apps.length) {
   try {
     admin.initializeApp({
       credential: admin.credential.cert({
-        projectId: process.env.FIREBASE_PROJECT_ID,
-        clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+        projectId: process.env.FIREBASE_PROJECT_ID || '',
+        clientEmail: process.env.FIREBASE_CLIENT_EMAIL || '',
         privateKey: privateKey,
       }),
     });
